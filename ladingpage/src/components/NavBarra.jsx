9da399c8d110847ab91b6c6641ebc8b121b02f11
Gabriel Loureiro <loureirogabriel672@gmail.com>
2025-02-05 11:17:34 -0300

    Refazendo
    asd

--- ladingpage/src/components/NavBarra.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NavBarra() {
-  return (
-    <div>
-      <h1>Fazer Cadastro</h1>
-    </div>
-  )
-}
---
+++ ladingpg/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Home from './pages/home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+/* 
+<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+*/
