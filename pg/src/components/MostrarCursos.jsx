91ce95859926636543125ef81df796229070be45
Gabriel Loureiro <loureirogabriel672@gmail.com>
2025-02-07 08:09:20 -0300

    asd

--- pg/src/components/MostrarCursos.jsx
+++ pg/src/components/MostrarCursos.jsx
@@ -2,6 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React, { useEffect, useState } from 'react';
 import CursosData from '../database/db.json';  // Caminho correto para o seu arquivo db.json
+import { Link } from 'react-router-dom';
 
 export default function MostrarCursos() {
   const [cursos, setCursos] = useState([]);
@@ -13,8 +14,8 @@ export default function MostrarCursos() {
   return (
     <div style={{display:'flex', justifyContent: 'center',}}>
       {cursos.map((curso, index) => (  // Itera sobre o array de cursos
-        <Card id='cartao' key={index} style={{ width: "18rem", marginBottom: "20px", backgroundColor: "", padding:"0px 20px",}}>
-                    <Card.Img 
+        <Card id='cartao' key={index} style={{ width: "18rem", marginBottom: "20px", backgroundColor: "", padding:"0px 20px"}}>
+          <Card.Img 
             variant="top" 
             src={curso.foto}
             style={{ width: '100%', height: 'auto' }} 
@@ -24,8 +25,10 @@ export default function MostrarCursos() {
             <Card.Text>
               {curso.historia}
             </Card.Text>
+            <Link to="/curso.nome/id">
             <Button variant="primary"
             style={{padding: "8px 60px"}}>Ir</Button>
+            </Link>
           </Card.Body>
         </Card>
       ))}
